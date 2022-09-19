2220372201881bf6ac6a2728458ed5d9fca5bba4
sebastiansilveira <sebastiansilveirasueta@gmail.com>
2022-09-19 10:11:22 -0300

    BaSP-W05-2022: exercise 04

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -5,6 +5,17 @@ console.log('---EXERCISE 4: IF ELSE');
 
 console.log ('-Excercise 4.a');
 
+var randomNumber;
+
+randomNumber = Math.random(2);
+
+if(randomNumber <= 0.5) {
+    console.log(randomNumber + ' ' + 'Lower than 0.5');
+}
+else{
+    console.log(randomNumber + ' ' + 'Greater than 0.5');
+}
+
 // b. Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
 // i.   “Bebe” si la edad es menor a 2 años;
 // ii.  “Niño” si la edad es entre 2 y 12 años;
@@ -15,3 +26,29 @@ console.log ('-Excercise 4.a');
 // vii. “Anciano” si es mayor a 75 años.
 
 console.log ('-Excercise 4.b');
+
+var age;
+
+age = Math.floor(Math.random() * 100);
+
+if(age < 2) {
+    console.log(age + ' ' + 'Baby');
+}
+else if ((age >= 2) && (age < 13)) {
+    console.log(age + ' ' + 'Child');
+}
+else if ((age >= 13) && (age < 20)) {
+    console.log(age + ' ' + 'Teenager');
+}
+else if ((age >= 20) && (age < 31)) {
+    console.log(age + ' ' + 'Young Adult');
+}
+else if ((age >= 31) && (age < 61)) {
+    console.log(age + ' ' + 'Adult');
+}
+else if ((age >= 61) && (age < 76)) {
+    console.log(age + ' ' + 'Elderly');
+}
+else if (age >= 76) {
+    console.log(age + ' ' + 'Elder');
+}
