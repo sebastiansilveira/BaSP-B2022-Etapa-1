984d82bb7b6546f4c771cb703dae691bb5bb10f4
sebastiansilveira <sebastiansilveirasueta@gmail.com>
2022-09-18 23:02:09 -0300

    BaSP-W05-2022: exercise 02

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -3,7 +3,7 @@ console.log('---EXERCISE 1: VARIABLES AND OPERATORS');
 // a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números
 //    en una tercera variable.
 
-console.log ('-Excercise 1.a');
+console.log('-Excercise 1.a');
 
 var liquidSalary, bonus, fullSalary;
 
@@ -15,20 +15,20 @@ console.log(fullSalary);
 
 // b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una tercera variable.
 
-console.log ('-Excercise 1.b');
+console.log('-Excercise 1.b');
 
 var firstName, lastName, fullName;
 
-firstName = 'Sebastian ';
+firstName = 'Sebastian';
 lastName = 'Silveira';
 fullName = firstName + lastName;
 
-console.log('The full name of the student is ' + firstName + lastName);
+console.log('The full name of the student is' + ' ' + firstName + ' ' + lastName);
 
 // c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string),
 //    guardando el resultado de la suma en una tercera variable, (utilizar lenght).
 
-console.log ('-Excercise 1.c');
+console.log('-Excercise 1.c');
 
 var food, drink, numberOfLetters;
 
@@ -36,4 +36,4 @@ food = 'Hamburger';
 drink = 'Beer';
 numberOfLetters = food.length + drink.length;
 
-console.log('The number of letters of the two strings combined is ' + numberOfLetters);
+console.log('The number of letters of the two strings combined is'+ ' ' + numberOfLetters);
--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -3,32 +3,67 @@ console.log('---EXERCISE 2: STRINGS');
 // a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula,
 //    (utilizar toUpperCase).
 
-console.log ('-Excercise 2.a');
+console.log('-Excercise 2.a');
+
+var unmarried;
+
+unmarried = 'bachelorette';
+
+console.log(unmarried.toUpperCase());
 
 // b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5
 //    caracteres guardando el resultado en una nueva variable, (utilizar substring).
 
-console.log ('-Excercise 2.b');
+console.log('-Excercise 2.b');
+
+var word;
+
+word = 'fabrication';
+
+console.log('The first five letters of the word fabrication are ' + word.substring(0,5));
 
 // c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3
 //    caracteres guardando el resultado en una variable, (utilizar substring).
 
-console.log ('-Excercise 2.c');
+console.log('-Excercise 2.c');
+
+var country;
+
+country = 'Bangladesh';
+
+console.log('The last three letters of Bangladesh are ' + country.substring(7,10));
 
 // d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra
 //    mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable,
 //    (utilizar substring, toUpperCase, toLowerCase y el operador +).
 
-console.log ('-Excercise 2.d');
+console.log('-Excercise 2.d');
+
+var phrase;
+
+phrase = 'abandonment';
+
+console.log(phrase.substring(0,1).toUpperCase() + phrase.substring(1).toLowerCase());
 
 // e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición
 //    del primer espacio en blanco y guardarla en una variable, (utilizar indexOf).
 
-console.log ('-Excercise 2.e');
+console.log('-Excercise 2.e');
+
+var fingerprint;
+
+fingerprint = 'dactylogram ';
+
+console.log(fingerprint.indexOf(' '));
 
 // f. Crear una variable de tipo string con al menos 2 palabras largas, (10 caracteres y algún espacio entre medio).
 //    Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de
 //    ambas palabras en mayúscula y las demás letras en minúscula,
 //    (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
 
-console.log ('-Excercise 2.f');
+console.log('-Excercise 2.f');
+
+var places;
+
+places = 'madagascar uzbekistan';
+
